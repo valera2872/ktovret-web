0e6bfa54547eed0efe984dd1985ca7231cffd4da
valera2872 <barbarov28@protonmail.com>
2026-08-06 08:39:47 +0300

    fix: align stored progress keys

--- assets/dossier-progress.js
+++ assets/dossier-progress.js
@@ -7,10 +7,10 @@
   const siteRoot = new URL('../', currentScript.src);
   const cases = [
     { number: '001', title: 'Четыре входа в архив', storageKey: 'ktovret:web:demo:v4:first_r3_001_four_archive_entries', path: 'delo/chetyre-vhoda-v-arhiv/' },
-    { number: '002', title: 'Три несинхронных журнала', storageKey: 'ktovret:web:demo:v4:first_r3_002_three_unsynced_logs', path: 'delo/tri-nesinhronnyh-zhurnala/' },
-    { number: '003', title: 'Пять папок и пустое место', storageKey: 'ktovret:web:demo:v4:first_r3_003_five_folders_empty_slot', path: 'delo/pyat-papok-i-pustoe-mesto/' },
+    { number: '002', title: 'Три несинхронных журнала', storageKey: 'ktovret:web:demo:v4:first_r3_002_unsynced_logs', path: 'delo/tri-nesinhronnyh-zhurnala/' },
+    { number: '003', title: 'Пять папок и пустое место', storageKey: 'ktovret:web:demo:v4:first_r3_003_five_folders_gap', path: 'delo/pyat-papok-i-pustoe-mesto/' },
     { number: '004', title: 'Ноутбук у двух выходов', storageKey: 'ktovret:web:demo:v4:first_r3_004_laptop_two_exits', path: 'delo/noutbuk-u-dvuh-vyhodov/' },
-    { number: '005', title: 'Карта, телефон и восемь минут', storageKey: 'ktovret:web:demo:v4:first_r3_005_card_phone_eight_minutes', path: 'delo/karta-telefon-i-vosem-minut/' },
+    { number: '005', title: 'Карта, телефон и восемь минут', storageKey: 'ktovret:web:demo:v4:first_r3_005_card_phone_route', path: 'delo/karta-telefon-i-vosem-minut/' },
     { number: '066', title: 'Запись до вскрытия контейнера', storageKey: 'ktovret:web:demo:v3:volume1_066', path: 'delo/zapis-do-vskrytiya-konteynera/' },
   ];
 
