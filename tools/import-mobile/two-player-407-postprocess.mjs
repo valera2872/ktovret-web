0bad146dac1dd7274392f36b1094d837bac6ea45
valera2872 <barbarov28@protonmail.com>
2026-08-23 21:37:05 +0300

    chore: bump Room 407 logic assets to 1.2.0

--- tools/import-mobile/two-player-407-postprocess.mjs
+++ tools/import-mobile/two-player-407-postprocess.mjs
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { ensureDir } from './common.mjs';
 
-const VERSION = '1.1.0';
+const VERSION = '1.2.0';
 const LANDING = 'detektivnye-igry-dlya-dvoih/index.html';
 const CASE_ROUTE = 'detektivnye-igry-dlya-dvoih/407';
 
