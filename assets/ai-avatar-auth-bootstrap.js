950ed09cdbf86452a5520ffe807eb2827f10fb9f
valera2872 <barbarov28@protonmail.com>
2026-08-30 16:34:02 +0300

    Bootstrap valid public auth before legacy AvatarBridge

---
+++ assets/ai-avatar-auth-bootstrap.js
@@ -0,0 +1,6 @@
+(()=>{
+  'use strict';
+  const PUBLIC_ANON='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ya252dXdrbnZzZWRqZ3FjZndjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxOTY2MzcsImV4cCI6MjEwMTc3MjYzN30.68loNx8A71dodfOXXKs_-I235XVCmEioXGrg8kCZQr4';
+  window.ML_AI_PUBLIC_AUTH={...(window.ML_AI_PUBLIC_AUTH||{}),anon:PUBLIC_ANON};
+  window.ML_AVATAR_CONFIG={...(window.ML_AVATAR_CONFIG||{}),publicAnon:PUBLIC_ANON};
+})();
